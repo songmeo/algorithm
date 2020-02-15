cb9decc7f08a73a39c6131a5517541b31b7bf38d
songmeo <shuong1012@gmail.com>
2020-02-15 19:31:35 +0200

    binary search

---
+++ binary_search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+template <typename Comparable>
+Comparable binary_search(std::vector<Comparable> v, Comparable k) {
+  Comparable min = 0, max = v.size() - 1, mid;
+  while(v[min] <= v[max]) {
+    mid = (max + min) / 2;
+    if(k == v[mid]) 
+      return mid; 
+    else if(k < v[mid])
+      max = mid - 1;
+    else
+      min = mid + 1;      
+  }
+  return -1;
+}
+
+int main() {
+  std::vector<int> v{200, 0, 9, 10, 500, 300};
+  std::cout << binary_search(v, 9) << std::endl;
+  return 0;
+}
