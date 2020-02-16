b8afafdbee9027cddcf0452aa6e60174f024e66a
songmeo <shuong1012@gmail.com>
2020-02-16 14:13:19 +0200

    changes binary search

--- binary_insertion_sort.cpp
+++ binary_insertion_sort.cpp
@@ -2,30 +2,27 @@
 #include <vector>
 
 template <typename Comparable>
-int binary_search(std::vector<Comparable> v, Comparable k) {
-  int min = 0, max = v.size() - 1, mid;
-  while(max > min) {
-    mid = (max + min) / 2;
-    if(k == v[mid]) 
-      return mid; 
+int binary_search(std::vector<Comparable> v, Comparable k, int max, int min) {
+    if(max == min) 
+      return (k > v[min])?(min + 1):min;
+    int mid = (max + min) / 2;
+    if(k == v[mid])
+      return mid;
     else if(k < v[mid])
-      max = mid - 1;
+      return binary_search(v, k, mid - 1, min);
     else
-      min = mid + 1;      
-  }
-  return (max + min)/2;
+      return binary_search(v, k, max, mid + 1);
 }
 
 template <typename Comparable>
 void binary_insertion_sort(std::vector<Comparable> v) {
-  auto b = v.begin();
-  for(int i = 1; i < v.size(); i++) {
-    auto e = v.begin() + i - 1;
-    std::vector<Comparable> tmp_vec(b,e);
-    Comparable tmp = std::move(v[i]);
-    int l = binary_search(tmp_vec, tmp);
-    if(v[l] > tmp) v.insert(b + l, tmp);
-    else v.insert(b + l + 1, tmp); 
+  for(int i = 0; i < v.size(); i++) {
+    Comparable tmp = std::move(v[i + 1]);
+    int l = binary_search(v, tmp, i, 0);
+    for(int j = i + 1; j > 0; --j) {
+      v[j] = std::move(v[j - 1]);
+    }
+    v[l] = std::move(tmp);
   }
 }
 
