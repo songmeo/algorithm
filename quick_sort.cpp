c58d3c83fa10d1938910edc700787a1d80cf08a4
songmeo <shuong1012@gmail.com>
2020-02-18 15:11:00 +0200

    add note

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <vector>
-    
+
+/*
+the idea is choosing a pivot, dividing into smaller than pivot and larger than pivot group,
+doing the same with those 2 groups until group element become 0.
+The first replacement to the real vector starts when smaller group has only 1 element left, which
+is also the smallest item. It is then placed to the beginning of the vector, continuing with
+same and larger groups. Because we know how many elements actually smaller and the same with pivot
+we can place the elements in the right place without having to know those before it!
+It's like swapping places between smaller and larger groups.
+*/
 template <typename Comparable>
 void quick_sort(std::vector<Comparable> &v) {
   if(v.size() > 0) {
-     std::vector<Comparable> smaller;
+    std::vector<Comparable> smaller;
     std::vector<Comparable> same;
     std::vector<Comparable> larger;
     Comparable pivot = v[v.size()/2];
