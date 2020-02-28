9326efa0529cba74f236d191fa1ab15032d26601
songmeo <shuong1012@gmail.com>
2020-02-28 17:49:55 +0200

    make a binary tree from an array

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -5,13 +5,21 @@ struct TreeNode {
   int val;
   TreeNode *left;
   TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {} 
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-TreeNode* insertLevelOrder(int arr[], TreeNode* root, int i, int n) {
-  if(i < n) { 
-    TreeNode* tmp(arr[i]);
+TreeNode* insert(int arr[], TreeNode* root, int i, int n) {
+  if(i < n) {
+    TreeNode* tmp = new TreeNode(arr[i]);
     root = tmp;
+    root->left = insert(arr,root->left, 2*i + 1, n);
+    root->right = insert(arr,root->right, 2*i + 2, n);
   }
+  return root;
 }
 
+int main() {
+	int arr[] = {1,2,2,3,4,4,3};
+	TreeNode* root = insert(arr, root, 0, sizeof(int));
+	return 0;
+}
