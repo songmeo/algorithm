fd23d4aea4804f9c42f2698f623e007b76170b02
songmeo <shuong1012@gmail.com>
2020-02-29 19:33:43 +0200

    queue using linked list

--- queue.cpp
+++ queue.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
-
-//implement queue using linked list
+using namespace std;
+/*
+ * implement queue using linked list
+ * enqueue from the back and dequeue from the front
+ * both must be operated with time complexity O(1)
+ */
 struct Node {
 	int data;
 	Node* next;
-}
+	Node(int x): data(x), next(NULL) {}
+};
 
 class Queue {
 public:
 	Node* front;
 	Node* rear;
-	enqueue() {
-	
+	Queue() {
+		front = rear = NULL;
+	}
+	void enqueue(int i) {
+		Node* n = new Node(i);
+		n->next = NULL;
+		if(!front && !rear) front = rear = n;
+		else {
+			rear->next = n;
+			rear = n;
+		}
 	}
 	
-	dequeue() {
+	void dequeue() {
+		front = front->next;
 	}
+	
+	void display() {
+		if(!front && !rear) cout << "empty queue";
+		else {
+			Node* tmp = front;
+			while(tmp) {
+				cout << tmp->data << " ";
+				tmp = tmp->next;
+			}
+		}
+	}
+	
+	void peek() {
+		cout << front->data;
+	}
+};
+
+int main() {
+	Queue q;
+    q.enqueue(5);
+    q.enqueue(0);
+    q.enqueue(-3);
+    q.display();
+    q.dequeue();
+    q.peek();
+    return 0;
 }
