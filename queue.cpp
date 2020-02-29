824d5026aa1ebea4d5a50fe3a942d98c6ee662b9
songmeo <shuong1012@gmail.com>
2020-02-29 17:46:54 +0200

    queue

---
+++ queue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//implement queue using linked list
+struct Node {
+	int data;
+	Node* next;
+}
+
+class Queue {
+public:
+	Node* front;
+	Node* rear;
+	enqueue() {
+	
+	}
+	
+	dequeue() {
+	}
+}
