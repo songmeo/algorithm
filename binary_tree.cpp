738c03025f4e823507a613b932216eff30c762f4
songmeo <shuong1012@gmail.com>
2020-03-01 08:52:03 +0200

    fix name

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -8,12 +8,12 @@ struct TreeNode {
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-TreeNode* insert(int arr[], TreeNode* root, int i, int n) {
+TreeNode* insertLevelOrder(int arr[], TreeNode* root, int i, int n) {
   if(i < n) {
     TreeNode* tmp = new TreeNode(arr[i]);
     root = tmp;
-    root->left = insert(arr,root->left, 2*i + 1, n);
-    root->right = insert(arr,root->right, 2*i + 2, n);
+    root->left = insertLevelOrder(arr,root->left, 2*i + 1, n);
+    root->right = insertLevelOrder(arr,root->right, 2*i + 2, n);
   }
   return root;
 }
@@ -47,7 +47,7 @@ void printPostOrder(TreeNode* root) {
 
 int main() {
 	int arr[] = {1,2,2,3,4,4,3};
-	TreeNode* root = insert(arr, root, 0, sizeof(arr)/sizeof(arr[0]));
+	TreeNode* root = insertLevelOrder(arr, root, 0, sizeof(arr)/sizeof(arr[0]));
 	cout << "InOrder: ";
 	printInOrder(root);
 	cout << endl;
