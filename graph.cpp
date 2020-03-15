656ba3761376acd688df0e1401a793d9bab942e4
songmeo <shuong1012@gmail.com>
2020-03-15 20:02:10 +0200

    time complexity analysis

--- graph.cpp
+++ graph.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
+/* 
+ * undirected weighted graph
+ * edge list: space complexity O(n) (start and end vertexes are presented as reference to the vertext list
+ * so the cost for every elements is the same)
+ * finding all adjacent vertexes: time complexity O(n) (linear search through edge list for a certain vertex)
+ * finding if 2 vertexes are connected: time complexity 0(n) (linear search through edge list)
+*/
 struct Edge {
-	char* start;
-	char* end;
+	int startV; 
+	int endV;
+	int weight;
 };
+
 
 int main() {
 	vector<Edge* > EdgeList;
