c7d8287322dc7f53e350fdf39ac2fba3c4c08f18
songmeo <shuong1012@gmail.com>
2020-03-28 19:50:32 +0200

    add bfs

--- bfs.cpp
+++ bfs.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <queue>
+#include <list>
 using namespace std;
 
 //bfs on a binary tree is like level order, dfs is like preorder
 class Graph {
 private:
 	int V;
-	vector<list<int>> adj;
+	vector<list<int>> *adj;
 public:
 	Graph(int v);
 	void addEdge(int v, int w);
@@ -18,11 +19,13 @@ Graph::Graph(int v) {
 	adj = new queue<int>[V];
 }
 
-struct Node {
-	char data;
-	Node* next;
-	Node(char c): data(c), next(NULL) {}
+Graph::addEdge(int v, int w) {
+	adj[v].push_back(w);
 }
 
-int main() {
+void Graph::BFS(int s) {
+	vector<bool> visited(V);
+	queue<int> q;
+	visited[s] = true;
+	queue.push(s);
 }
