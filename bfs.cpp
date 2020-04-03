f96570d1415e789adc0c6db2830340879e39d06e
songmeo <shuong1012@gmail.com>
2020-04-03 12:05:52 +0300

    done

--- bfs.cpp
+++ bfs.cpp
@@ -6,26 +6,50 @@ using namespace std;
 //bfs on a binary tree is like level order, dfs is like preorder
 class Graph {
 private:
-	int V;
-	vector<list<int>> *adj;
+	int vertices;
+	list<int> *adj;
 public:
-	Graph(int v);
+	Graph(int i);
 	void addEdge(int v, int w);
 	void BFS(int s);
 };
 
-Graph::Graph(int v) {
-	V = v;
-	adj = new queue<int>[V];
+Graph::Graph(int i) {
+	vertices = i;
+	adj = new list<int>[i];
 }
 
-Graph::addEdge(int v, int w) {
+void Graph::addEdge(int v, int w) {
 	adj[v].push_back(w);
 }
 
 void Graph::BFS(int s) {
-	vector<bool> visited(V);
-	queue<int> q;
-	visited[s] = true;
-	queue.push(s);
+	bool visited[vertices] = { false }; // mark all vertices as not visited
+	queue<int> q; //a queue for bfs
+	visited[s] = true; //mark s as visited node
+	q.push(s); //enqueue s
+	list<int>::iterator i;
+	while(!q.empty()) {
+		s = q.front();
+		cout << s << " ";
+		q.pop();
+		for(i = adj[s].begin(); i != adj[s].end(); ++i) {
+			if(!visited[*i]) {
+				visited[*i] = true;
+				q.push(*i);
+			}
+		}
+	}
+}
+
+int main() {
+	Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
+	g.BFS(2);
+	return 0;
 }
