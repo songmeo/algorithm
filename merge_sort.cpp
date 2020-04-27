7bd4b8be0478ec21aab8a36b6859f9a2a2998c70
songmeo <shuong1012@gmail.com>
2020-04-27 12:16:06 +0300

    ans vector

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -5,39 +5,40 @@ using namespace std;
 /*
  * merge sort using divide and conquer
  */
-void merge(vector<int>& v, int lo, int mid, int hi) {
-	vector<int> left(v.begin() + lo, v.begin() + mid);
-	vector<int> right(v.begin() + mid + 1, v.begin() + hi);
-	int l = 0, r = 0, i = 0;
-	while(l < left.size() && r < right.size()) {
-		if(left[l] < right[r]) {
-			v[i++] = left[l++];
+void merge(vector<int>& v, vector<int>& ans, int lo, int mid, int hi) {
+	int l = lo, r = mid + 1, k = lo;
+	while(l <= mid && r <= hi) {
+		if(v[l] <= v[r]) {
+			ans[k++] = v[l++];
 		}
 		else {
-			v[i++] = right[r++];
+			ans[k++] = v[r++];
 		}
 	}
-	for(; l < left.size(); l++) {
-		v[i++] = left[l];
+	while(r <= hi) {
+		ans[k++] = v[r++];
 	}
-	for(; r < right.size(); r++) {
-		v[i++] = right[r];
+	while(l <= mid) {
+		ans[k++] = v[l++];
 	}
 }
 
-void mergesort(vector<int>& v, int l, int h) {
+void mergesort(vector<int>& v, vector<int>& ans, int l, int h) {
 	if(l < h) {
 		int mid = (l + h) / 2;
-		mergesort(v, l, mid);
-		mergesort(v, mid + 1, h);
-		merge(v, l, mid, h);
+		mergesort(v, ans, l, mid);
+		mergesort(v, ans, mid + 1, h);
+		merge(v, ans, l, mid, h);
 	}
 }
+
+
 
 int main() {
 	vector<int> v{1,2,6,3,9,8,7,10};
-	mergesort(v, 0, v.size() - 1);
-	for(int i : v)
+	vector<int> ans(v.size());
+	mergesort(v, ans, 0, v.size() - 1);
+	for(int i : ans)
 		cout << i << " ";
 	return 0;
 }
