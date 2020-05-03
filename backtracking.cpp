8890d34fa692ab5e578281f1221ce2c73c1be31f
songmeo <shuong1012@gmail.com>
2020-05-03 20:36:22 +0300

    initial

--- backtracking.cpp
+++ backtracking.cpp
@@ -1,25 +1,46 @@
 /*
- * backtracking solution begins with an empty solution and extends it
- * step by step. The search recursively goes through all different ways
- * how a solution can be constructed.
- * 
  * using n-queen problem
  */
 #include <iostream>
 #include <vector>
 using namespace std;
 
+class pos {
+	int x, y;
+	pos(int x, int y) : x(x), y(y) {}
+};
+
 class Solution {
+private:
+	int size;
 public:
-	int n_queen(int n) {
-		int solutions = 0;
-		vector<vector<int>> v(n);
-		return 1;
+	Solution(int s) {
+		size = s;
+	}
+	
+	bool attacked(vector<pos> positions, pos p) {
+		for(pos i : positions) {
+			if(p.x == i.x || p.y == i.y || i.x + i.y == p.x + p.y || p.x - i.x == p.y - i.y) 
+				return true;
+		}
+		return false;
+	}
+	
+	int n_queen {
+		int ans = 0;
+		vector<pos> positions;
+		//place a queen on each row
+		int r = 0, c = 0;
+		if(p.x == i.x || p.y == i.y || i.x + i.y == p.x + p.y || p.x - i.x == p.y - i.y) 
+				return true;
+		else
+			c++;
+		return ans;
 	}
 };
 
 int main() {
-	Solution s;
-	cout << s.n_queen(4);
+	Solution s(4);
+	cout << s.n_queen;
 	return 0;
 }
